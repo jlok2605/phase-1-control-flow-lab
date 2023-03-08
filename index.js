78fd90fae95034d23368a766474a4ed9cd870540
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-03-08 14:51:48 +1000

    Assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let ridePrice
+
+  if (distance < 400)
+    ridePrice = 'This one is on me!'
+  else if (distance < 2000)
+    ridePrice = 'That will be twenty bucks.'
+  else if (distance < 2500)
+    ridePrice = 'I will gladly take your thirty bucks.'
+  else ridePrice = 'No can do.'
+  return ridePrice
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(3000)
+
+
+
+function ternaryCheckCity(city) {
+  const cityMessage = (city === 'NYC') ? "Ok, sounds good." : "No go."
+  return cityMessage
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ternaryCheckCity('Brisbane')
+
+function switchOnCharmFromTip(tip) {
+  let isGenerous;
+  switch (tip) {
+    case 'generous':
+      isGenerous = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      isGenerous = 'Thank you.'
+      break;
+    default:
+      isGenerous = 'Bye.'
+  }
+  return isGenerous
 }
+
+switchOnCharmFromTip('not as generous')
+
+
+
+
